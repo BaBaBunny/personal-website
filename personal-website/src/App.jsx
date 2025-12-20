95a7e7b1e9985272a362aaa5d74a657b4f726000
BaBaBunny <lucas123p71@gmail.com>
2025-12-19 22:00:32 -0500

    BABABUNNY

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,14 +1,17 @@
 import NavBar from './NavBar.jsx'
+import Body from './Body.jsx'
+import './App.css';
 
 function App() {
 
   return(
     <div>
+      <Body/>
       <NavBar/>
     </div>
 
   );
-  
+
 }
 
 export default App
