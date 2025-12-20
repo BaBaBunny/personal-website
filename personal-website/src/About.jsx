5e25e7b9288bd0709f2adef13b92cecae97d12ca
BaBaBunny <lucas123p71@gmail.com>
2025-12-19 22:50:23 -0500

    pages

---
+++ personal-website/src/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <section id = "about" className = "page-section">
+            <h2>About Me</h2>
+            <p>low taper fade</p>
+        </section>
+    );
+}
+
+export default About
--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,16 +1,15 @@
 import NavBar from './NavBar.jsx'
 import Body from './Body.jsx'
+import About from './About.jsx'
 
 function App() {
-
   return(
     <div>
       <NavBar/>
       <Body/>
+      <About/>
     </div>
-
   );
-
 }
 
 export default App
