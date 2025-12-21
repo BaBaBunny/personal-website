95062747486898bafdce1bdcd023eafb5b96d741
BaBaBunny <lucas123p71@gmail.com>
2025-12-20 21:33:00 -0500

    box shadows and about contents

--- personal-website/src/About.jsx
+++ personal-website/src/About.jsx
@@ -1,8 +1,20 @@
+import profilePic from './assets/bababunny.jpg';
+
 function About() {
     return (
         <section id="about" className="page-section">
             <h1>About Me</h1>
-            <p>all about me</p>
+            <div className="about-content">
+                <img src={profilePic} alt="Profile Picture" className="profile-pic"/>
+                <div className="about-text">
+                    <p>
+                        Hello! I'm Bababunny, a passionate developer with a love for creating beautiful and functional web applications. With a background in computer science and a keen eye for design, I strive to build user-friendly experiences that make a difference.
+                    </p>
+                    <p>
+                        When I'm not coding, you can find me exploring the outdoors, experimenting with new recipes in the kitchen, or indulging in my love for photography. I'm always eager to learn new technologies and take on exciting challenges.
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
