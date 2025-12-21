614711c512c8de6817ba0e910329ca031ecf9475
BaBaBunny <lucas123p71@gmail.com>
2025-12-21 14:42:19 -0500

    rotating home page text

--- personal-website/src/Home.jsx
+++ personal-website/src/Home.jsx
@@ -1,7 +1,22 @@
+import React, {useState, useEffect} from "react";
+
 function Home(){
+    const words = ["BABABUNNY", "MASSIVE", "WELCOME"];
+    const [currentWord, setCurrentWord] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentWord((prevWord) => (prevWord + 1) % words.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return(
-        <section className = "home-section">
-            <h1>BABABUNNY</h1>
+        <section id="home" className = "home-section">
+            <h1 key={currentWord} className="home-text">
+                {words[currentWord]}
+            </h1>
         </section>
     );
 }
