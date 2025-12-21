6737a64ada68054bf813d8299a7cce5aae9e1829
BaBaBunny <lucas123p71@gmail.com>
2025-12-20 22:33:24 -0500

    about-image

--- personal-website/src/About.jsx
+++ personal-website/src/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <section id="about" className="page-section">
             <h1>About Me</h1>
             <div className="about-content">
-                <img src={profilePic} alt="Profile Picture" className="profile-pic"/>
+                <img src={profilePic} alt="Profile Picture" className="about-image"/>
                 <div className="about-text">
                     <p>
                         Hello! I'm Bababunny, a passionate developer with a love for creating beautiful and functional web applications. With a background in computer science and a keen eye for design, I strive to build user-friendly experiences that make a difference.
