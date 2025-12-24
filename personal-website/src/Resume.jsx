377360c63131f421f9768f24e895b07f46619588
BaBaBunny <lucas123p71@gmail.com>
2025-12-24 16:52:24 -0500

    asdas

--- personal-website/src/Resume.jsx
+++ personal-website/src/Resume.jsx
@@ -2,7 +2,20 @@ function Resume() {
     return (
         <section id = "resume" className = "page-section">
             <h1 className="title">Resume</h1>
-            <p>my skills and experience</p>
+            <div className="experience">
+                <h2>Experience</h2>
+                <p>my skills and experience</p>
+            </div>
+            <div>
+                <div className="education">
+                    <h2>Education</h2>
+                    <p>my educational background</p>
+                </div>
+                <div className="skills">
+                    <h2>Skills</h2>
+                    <p>my skills</p>
+                </div> 
+            </div>
         </section>
     );
 }
