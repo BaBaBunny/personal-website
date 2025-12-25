61f29a5e9c651975f8db0a6cd67e5e8ae5f94648
BaBaBunny <lucas123p71@gmail.com>
2025-12-24 21:48:23 -0500

    asds

--- personal-website/src/Resume.jsx
+++ personal-website/src/Resume.jsx
@@ -2,19 +2,21 @@ function Resume() {
     return (
         <section id = "resume" className = "page-section">
             <h1 className="title">Resume</h1>
-            <div className="experience">
-                <h2>Experience</h2>
-                <p>my skills and experience</p>
-            </div>
-            <div>
-                <div className="education">
-                    <h2>Education</h2>
-                    <p>my educational background</p>
+            <div className="resume-grid">
+                <div className="resume-left">
+                    <h2>Experience</h2>
+                    <p>my skills and experience</p>
                 </div>
-                <div className="skills">
-                    <h2>Skills</h2>
-                    <p>my skills</p>
-                </div> 
+                <div className="resume-right">
+                    <div className="resume-top">
+                        <h2>Education</h2>
+                        <p>my educational background</p>
+                    </div>
+                    <div className="resume-bottom">
+                        <h2>Skills</h2>
+                        <p>my skills</p>
+                    </div> 
+                </div>
             </div>
         </section>
     );
