760d5c619206cd725c4c85db890f164782823e7d
BaBaBunny <lucas123p71@gmail.com>
2025-12-26 01:02:15 -0500

    asdsd

--- personal-website/src/Projects.jsx
+++ personal-website/src/Projects.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 function Projects() {
     return (
-        <section className="page-section">
-            <h1 className="title">Arts & Crafts</h1>
-            <hr className="divider" />
-            <div className="container">
-                <p>My cool art goes here!</p>
-            </div>
-        </section>
+        <div className="page-section">
+            <h1>Projects Page</h1>
+        </div>
     );    
 }
 
