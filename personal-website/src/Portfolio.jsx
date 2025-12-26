87874e8f1ad361544878b2e9e91fac5baf9ea7c1
BaBaBunny <lucas123p71@gmail.com>
2025-12-25 22:32:48 -0500

    portfolio placeholder images

--- personal-website/src/Portfolio.jsx
+++ personal-website/src/Portfolio.jsx
@@ -1,20 +1,25 @@
+import placeholder from './assets/placeholder.jpeg';
+
 function Portfolio() {
     return (
         <section id = "portfolio" className = "page-section">
             <h1 className="title">Portfolio</h1>
             <div className="portfolio-grid">
                 <div className="portfolio-left">
+                    <img src={placeholder} alt="Placeholder" className="portfolio-featured"/>
                     <h2>Current Projects</h2>
-                    <p>my featured</p>
+                    <p className="subtitle">projects im currently working on</p>
                 </div>
                 <div className="portfolio-right">
                     <div className="portfolio-top">
                         <h2>Programming Related Projects</h2>
-                        <p>my past work</p>
+                        <p className="subtitle">any sort of projects related to programming</p>
+                        <img src={placeholder} alt="Placeholder" className="portfolio-image"/>
                     </div>
                     <div className="portfolio-bottom">
+                        <img src={placeholder} alt="Placeholder" className="portfolio-image"/>
                         <h2>Arts & Crafts</h2>
-                        <p>my links</p>
+                        <p className="subtitle">other projects not-related to programming</p>
                     </div> 
                 </div>
             </div>
