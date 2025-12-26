0331863b819679c54a50c05d7527d8aad6251676
BaBaBunny <lucas123p71@gmail.com>
2025-12-25 22:18:03 -0500

    portfolio

--- personal-website/src/Portfolio.jsx
+++ personal-website/src/Portfolio.jsx
@@ -1,8 +1,23 @@
 function Portfolio() {
     return (
         <section id = "portfolio" className = "page-section">
-            <h1>Portfolio</h1>
-            <p>my projects and creations</p>
+            <h1 className="title">Portfolio</h1>
+            <div className="portfolio-grid">
+                <div className="portfolio-left">
+                    <h2>Current Projects</h2>
+                    <p>my featured</p>
+                </div>
+                <div className="portfolio-right">
+                    <div className="portfolio-top">
+                        <h2>Programming Related Projects</h2>
+                        <p>my past work</p>
+                    </div>
+                    <div className="portfolio-bottom">
+                        <h2>Arts & Crafts</h2>
+                        <p>my links</p>
+                    </div> 
+                </div>
+            </div>
         </section>
     );
 }
