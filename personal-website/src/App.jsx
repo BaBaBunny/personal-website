5277c9c790523d73dfc74d76bc60fd5644b6ea26
BaBaBunny <lucas123p71@gmail.com>
2025-12-29 11:34:41 -0500

    asdsd

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,4 +1,3 @@
-import {Routes, Route, Link} from 'react-router-dom'
 import NavBar from './NavBar.jsx'
 import Home from './Home.jsx'
 import About from './About.jsx'
@@ -7,29 +6,33 @@ import Portfolio from './Portfolio.jsx'
 import Contact from './Contact.jsx'
 import Projects from './Projects.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return(
-    <>
-      <NavBar/>
-        <Routes>
-        <Route path="/" element={
-        <>
-          <Home/>
-          <hr className='divider'/>
-          <About/>
-          <hr className='divider'/>
-          <Resume/>
-          <hr className='divider'/>
-          <Portfolio/>
-          <hr className='divider'/>
-          <Contact/>
-        </>
-        } />
-        <Route path="/projects" element={<Projects/>} />
-      </Routes>
-    </>
+    <Router>
+      <div>
+        <NavBar/>
+          <div>
+            <Switch>
+              <Route path="/">
+                <Home/>
+              </Route>
+            </Switch>
+          </div>
+      </div>
+    </Router>
   );
 }
 
 export default App
+
+{/* <Home/>
+<hr className='divider'/>
+<About/>
+<hr className='divider'/>
+<Resume/>
+<hr className='divider'/>
+<Portfolio/>
+<hr className='divider'/>
+<Contact/> */}
