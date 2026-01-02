af11fd15099f9a958d2f2977538e5c1f5d084775
BaBaBunny <lucas123p71@gmail.com>
2026-01-02 18:26:42 -0500

    font style

--- personal-website/src/About.jsx
+++ personal-website/src/About.jsx
@@ -46,26 +46,31 @@ const About = () => {
                         className={`card front-card ${isShuffling ? 'yielding' : ''}`}
                     />
                 </div>
-                
+
                 <p>
                     Greetings and Salutations hires or anyone else who has stumbled upon this magnificent place.
-
+                </p>
+                <p>
                     I'm currently 2nd year undergraduate student, studying Computer Science at Queen's (or as they call it: School of Computing).
                     This website that you're on right now is the first thing I've ever made (during the 2025 winter break).
-
+                </p>
+                <p>
                     I like making stuff. I consider myself a relatively creative & imaginative person.
                     I'm one of those people who has a passion for arts but I don't really make that much stuff on my own.
                     BUT-I am looking to change that, with this website being the first stepping stone.
-
+                </p>
+                <p>
                     I have made SOME things, both physical arts and crafts and virtual creations, like animations and modelling through Blender.
                     I also have a YouTube channel (I haven't uploaded in nearly 2 years *whoopsie*) where I enjoy making *mostly* gameplay videos, with some occasional creative outputs (i need to start vlogging lol).
                     So essentially I know how to video edit, generate ideas, do 3D model, draw, animate, oh and music.
                     I play the guitar and violin! I'm also a huge film guy. I love movies (and other forms of visual media).
                     I also ski and snowboard (once so far), play tennis, badminton, swim, exercise, scroll reels and TikTok, and ponder the thoughts of the universe, etc...The usual y'know?
-
+                </p>
+                <p>
                     Anyway the point of this website is to-well act as a portfolio and resume to for employers to see (and hopefully hire me so I don't become homeless in 2 years). But not only just that, but also a way for me to display things I've created-no matter what it is-on one platform. Which will be this site. So as time passes, this website will grow larger and larger, eventually (i hope) becoming large enough to be essentially an library of my life and its workings.
-                    
-                    (January 2nd, 2025)
+                </p>
+                <p>
+                    (as of January 2nd, 2025)
                 </p>
             </div>
         </section>
