74d92b2ccb9400f950e3e78b7047474e8cee7320
BaBaBunny <lucas123p71@gmail.com>
2026-01-02 23:43:22 -0500

    space

--- personal-website/src/About.jsx
+++ personal-website/src/About.jsx
@@ -55,14 +55,14 @@ const About = () => {
                         This website is actually the very first project I've ever built (officially)!
                     </p>
                     <p>
-                        <strong>I love making stuff.</strong> 
+                        <strong>I love making stuff. </strong>
                         Whether it's digital art, physical art, or video editing and content creation for my YouTube channel, I love the creative process.
                         I play the guitar and violin! And I'm also a huge film guy.
                         I love movies (and other forms of visual media).
                         Outside of my room, I enjoy skiing/snowboarding, playing tennis & badminton, or searching up new recipes to cook up in the kitchen.
                     </p>
                     <p>
-                        <strong>Why this?</strong> 
+                        <strong>Why this? </strong>
                         To act as a live portfolio to display my creations and experiences and-hopefully-help me get employed so I don't become homeless in 2 years.
                         And as a I learn and create more, I'm hoping that this site will grow into a library of wealth and knowledge.
                     </p>
