842893b800d1b046b0e0d92f2a34ef57980ad562
BaBaBunny <lucas123p71@gmail.com>
2026-01-04 00:15:05 -0500

    page fadings

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -7,6 +7,7 @@ import Portfolio from './Portfolio.jsx';
 import Contact from './Contact.jsx';
 import Projects from './Projects.jsx';
 import ArtsCrafts from './Arts&Crafts.jsx';
+import Fading from './Fading.jsx';
 import './index.css';
 
 function App() {
@@ -18,23 +19,33 @@ function App() {
         /* main page */
         <Route path="/" element={
           <>
-            <Home />
-            <hr className='divider'/>
-            <About />
-            <hr className='divider'/>
-            <Resume />
-            <hr className='divider'/>
-            <Portfolio />
-            <hr className='divider'/>
-            <Contact />
+            <Fading>
+              <Home />
+            </Fading>
+            <Fading>
+              <hr className='divider'/>
+              <About />
+            </Fading>
+            <Fading>
+              <hr className='divider'/>
+              <Resume />  
+            </Fading>
+            <Fading>
+              <hr className='divider'/>
+              <Portfolio />
+            </Fading>
+            <Fading>
+              <hr className='divider'/>
+              <Contact />
+            </Fading>
           </>
-        } />
+        }/>
         {/* separate pages */}
         <Route path="/projects" element={<Projects />} />
         <Route path="/arts-crafts" element={<ArtsCrafts />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ personal-website/src/Fading.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+export default function Fading({children}) {
+    const [isVisible, setIsVisible] = useState(false);
+    const domRef = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                //If it's on screen (isIntersecting), set visible to true
+                //If it leaves screen, set visible to false (fade out)
+                setIsVisible(entry.isIntersecting);
+            });
+        });
+
+        const {current} = domRef;
+        if (current) observer.observe(current);
+
+        return () => {
+            if (current) observer.unobserve(current);
+        };
+    }, []);
+
+    return (
+        <div
+            className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+            ref={domRef}
+        >
+            {children}
+        </div>
+    );
+};
