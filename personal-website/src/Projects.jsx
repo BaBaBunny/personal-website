b3d471b7d6de49b3569df4235d6767b15925ade9
BaBaBunny <lucas123p71@gmail.com>
2026-01-03 23:50:26 -0500

    asds

--- personal-website/src/Projects.jsx
+++ personal-website/src/Projects.jsx
@@ -3,13 +3,15 @@ import {Link} from 'react-router-dom'
 
 function Projects() {
     return (
-        <section className="page-section">
+        <>
             <Link to="/#portfolio" className="back-button">
-                <i className="fa solid fa-xmark"></i>
+                    <i className="fa solid fa-xmark"></i>
             </Link>
-            <h1 className="title">Projects</h1>
-            <p>Here is where you'll find of my program-related creations.</p>
-        </section>
+            <section className="page-section">
+                <h1 className="title">Projects</h1>
+                <p>Here is where you'll find of my program-related creations.</p>
+            </section>
+        </>
     );    
 }
 
