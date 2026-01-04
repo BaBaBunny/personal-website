8ea673a1a3218ebb3427cef82eedcf95655fab60
BaBaBunny <lucas123p71@gmail.com>
2026-01-03 23:57:03 -0500

    typo

--- personal-website/src/Arts&Crafts.jsx
+++ personal-website/src/Arts&Crafts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HasLink as Link} from 'react-router-hash-link';
+import {HashLink as Link} from 'react-router-hash-link';
 
 function ArtsCrafts() {
     return (
